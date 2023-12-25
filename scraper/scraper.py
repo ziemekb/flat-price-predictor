b0aef9530b821deec7a73f189a0f2fcb401848e2
Ziemek <ziemowit.baczewski@gmail.com>
2023-12-25 13:20:59 +0100

    add retrieve_data function

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,9 +1,51 @@
 from bs4 import BeautifulSoup
+import json
 import requests
 import re
 
 BASE_URL = "https://www.otodom.pl"
 LISTINGS_URL = BASE_URL + "/pl/wyniki/sprzedaz/mieszkanie/dolnoslaskie/wroclaw"
+
+def retrieve_data(url):
+    listing = requests.get(url)
+    listing_soup = BeautifulSoup(listing.content, "html.parser")
+    
+    #top_info = listing_soup.find("div", attrs={"data-testid" : "ad.top-information.table"})
+    script_tag = listing_soup.find('script', {'id': '__NEXT_DATA__'})
+    json_data = script_tag.contents[0] if script_tag else None
+
+    if json_data:
+        try:
+            data = json.loads(json_data)
+            props = data.get('props', {})
+            # extract page properties
+            page_props = props.get('pageProps', {})
+            # extract ad id
+            ad_id = page_props.get('id')
+            ad_data = page_props.get('ad', {})
+            # extract ad description
+            ad_description = ad_data.get('description')
+            
+            # extract 'target'; fields which interest us the most
+            target = ad_data.get('target')
+            
+            area = float(target.get('Area', 0))
+            #build_year = int(target.get('Build_year', 0)) 
+            #floors_num = int(target.get('Building_floors_num', 0))
+            #floor_num = target.get('Floor_no') # keep in mind strange formating e.g. ["floor_5"]
+            price = int(target.get('Price', 0))
+            rent = int(target.get('Rent', 0))
+
+            print(f"ID: {ad_id}")
+            #print(f"Description: {ad_description}")
+            print(f"Area: {area}")
+            print(f"Price: {price}")
+            print(f"Rent: {rent}")
+
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {e}")
+    else:
+        print("Script tag with id='__NEXT_DATA__' not found.")
 
 page = requests.get(LISTINGS_URL)
 soup = BeautifulSoup(page.content, "html.parser")
@@ -13,7 +55,7 @@ pages_soup = pages_soup.find_all("a", attrs={"data-cy" : re.compile("pagination.
 
 max_page = max(int(page_num.text) for page_num in pages_soup)
 
-print(max_page)
+#print(max_page)
 
 visited_listings = set()
 
@@ -25,7 +67,8 @@ for page_num in range(1, max_page + 1):
         link = tag["href"]
         if link not in visited_listings:
             visited_listings.add(link)
-            # retrieve_data(link) -- implement retrieve data
+            retrieve_data(BASE_URL + link) 
+            input()
 
-print(len(visited_listings))
+#print(len(visited_listings))
 
