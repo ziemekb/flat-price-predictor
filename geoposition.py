a9eab17ade884b15b873010f03fc058894c43816
Ziemek <ziemowit.baczewski@gmail.com>
2024-01-12 10:34:31 +0100

    add writing data to csv

--- geoposition.py
+++ geoposition.py
@@ -2,7 +2,7 @@ import geopandas
 import matplotlib.pyplot as plt
 from shapely.geometry import Point
 
-def classify_coor(latitude, longitude):
+def classify_coords(latitude, longitude):
     location = geopandas.GeoDataFrame(geometry=geopandas.points_from_xy([longitude], [latitude]), 
                                       crs="EPSG:4326")
     for idx, row in wroclaw.iterrows():
@@ -14,4 +14,4 @@ shp_path = r"wroclaw/borders.shp"
 wroclaw = geopandas.read_file(shp_path)
 wroclaw = wroclaw.to_crs("EPSG:4326")
 
-#print(classify_coor(51.1168551569616, 16.95513647328204))
+#print(classify_coords(51.1168551569616, 16.95513647328204))
--- scraper.py
+++ scraper.py
@@ -6,13 +6,14 @@ import re
 import csv
 import pandas as pd
 from math import inf
+from geoposition import classify_coords
 
 BASE_URL = "https://www.otodom.pl"
 LISTINGS_URL = BASE_URL + "/pl/wyniki/sprzedaz/mieszkanie/dolnoslaskie/wroclaw"
 
+# all available properties of properties
 PROPERTIES=['area', 'price', 'district', 'rent', 'market', 'garage', 'balcony']
-# returns pandas Series with default columns price, metric area 
-# and parameters specified in args
+
 def retrieve_listing_data(url, properties):
     listing = requests.get(url)
     listing_soup = BeautifulSoup(listing.content, "html.parser")
@@ -23,6 +24,8 @@ def retrieve_listing_data(url, properties):
 
     if json_data:
         try:
+            props_dict = dict()
+
             data = json.loads(json_data)
             props = data.get('props', {})
             # extract page properties
@@ -31,39 +34,46 @@ def retrieve_listing_data(url, properties):
             ad_id = page_props.get('id')
             ad_data = page_props.get('ad', {})
             # extract ad description
-            ad_description = ad_data.get('description')
+            # ad_description = ad_data.get('description')
             
             # extract 'target'; fields which interest us the most
             target = ad_data.get('target')
             
-            area = float(target.get('Area', 0))
-            #build_year = int(target.get('Build_year', 0)) 
-            #floors_num = int(target.get('Building_floors_num', 0))
-            #floor_num = target.get('Floor_no') # keep in mind strange formating e.g. ["floor_5"]
-            price = target.get('Price', 0)
-            rent = target.get('Rent', 0)
-
-            print(f"ID: {ad_id}")
-            #print(f"Description: {ad_description}")
-            print(f"Area: {area}")
-            print(f"Price: {price}")
-            print(f"Rent: {rent}")
-
-            characteristics = ad_data.get('characteristics')
-            market = None # secondary or primary -- wtórny lub pierwotny
-            for c in characteristics:
-                if c.get('key', None) == "market":
-                    market = c.get('value', None)
-                    break
-
-            print(f"Market: {market}")
+            props_dict['area'] = float(target.get('Area', 0))
+            #props_dict['build_year'] = int(target.get('Build_year', 0)) 
+            #props_dict['floors_num'] = int(target.get('Building_floors_num', 0))
+            #props_dict['floor_num'] = target.get('Floor_no') # keep in mind strange formating e.g. ["floor_5"]
+            props_dict['price'] = target.get('Price', 0)
+            props_dict['rent'] = target.get('Rent', 0)
+            
             location = ad_data.get('location')
             coords = location.get('coordinates')
 
             latitude = coords.get('latitude', 0.0)
             longitude = coords.get('longitude', 0.0)
-            print(f"Latitude: {latitude}")
-            print(f"Longitude: {longitude}")
+            props_dict['district'] = classify_coords(latitude, longitude)
+            #print(f"Latitude: {latitude}")
+            #print(f"Longitude: {longitude}")
+            print(f"District: {props_dict['district']}")
+
+            print(f"ID: {ad_id}")
+            #print(f"Description: {ad_description}")
+            print(f"Area: {props_dict['area']}")
+            print(f"Price: {props_dict['price']}")
+            print(f"Rent: {props_dict['rent']}")
+
+            characteristics = ad_data.get('characteristics')
+            market = None # secondary or primary -- wtórny lub pierwotny
+            for c in characteristics:
+                if c.get('key', None) == "market":
+                    props_dict['market'] = c.get('value', None)
+                    break
+
+            print(f"Market: {props_dict['market']}")
+            
+            data = [props_dict[p] for p in properties]
+
+            return data if all(data) else None
 
         except json.JSONDecodeError as e:
             print(f"Error decoding JSON: {e}")
@@ -94,8 +104,8 @@ def scrape_otodom(properties=None, listings_mx=None):
     visited_listings = set()
     lcounter = 0
 
-    with open(r"listings.csv", "a", newline="") as csv_file:
-        writer = csv.writer(csv_file, delimiter=",")
+    with open(r"listings.csv", "w", newline="") as csv_file:
+        writer = csv.writer(csv_file, delimiter=";")
         writer.writerow([p.capitalize() for p in properties])
         max_page = get_max_page(LISTINGS_URL)
         for page_num in range(1, max_page + 1):
@@ -112,8 +122,7 @@ def scrape_otodom(properties=None, listings_mx=None):
                     continue
                 lcounter += 1
                 writer.writerow(data)
-                # here write data to csv file
-                if lcounter > listings_mx:
+                if lcounter >= listings_mx:
                     return None 
             input()
 
@@ -121,7 +130,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Scrapes otodom website for flat listings")
     parser.add_argument('-p', '--properties', nargs='+', choices=PROPERTIES, 
                         help='allows choosing which properties to scrape')
-    parser.add_argument('-l', '--listings', type=int,
+    parser.add_argument('-l', '--listings', type=int, metavar = 'n',
                         help='allows specifying how many listing to scrapes')
     args = parser.parse_args()
     scrape_otodom(args.properties, args.listings)
