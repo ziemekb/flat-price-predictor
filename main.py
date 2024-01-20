aa5a372c2051ab9630b0d8ed8d763b5f80b2677b
Ziemek <ziemowit.baczewski@gmail.com>
2024-01-20 18:09:41 +0100

    add labels but leave them out for now

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ wroclaw = wroclaw.rename(columns=
                           'SHAPE_AREA' : 'ShapeArea',
                           'SHAPE_LEN'  : 'ShapeLen'})
 wroclaw = wroclaw.join(means, on='District', how='left')
+wroclaw['RepPt'] = wroclaw['geometry'].apply(lambda x: x.representative_point().coords[:])
+wroclaw['RepPt'] = [coords[0] for coords in wroclaw['RepPt']]
 
 ax = wroclaw.plot(column='PricePerSqM', 
             legend=True,
@@ -25,5 +27,18 @@ ax = wroclaw.plot(column='PricePerSqM',
             cmap='YlOrRd', 
             missing_kwds={"color": "lightgrey"})
 ax.set_axis_off()
+
+'''
+labels for the districts;
+add visibility on hovering or leave it out 
+for idx, row in wroclaw.iterrows():
+    txt = ax.text(row['RepPt'][0], 
+            row['RepPt'][1], 
+            s=row['District'], 
+            horizontalalignment='center', 
+            fontsize='x-small',
+            bbox={'facecolor': 'white', 'alpha':0.8, 'edgecolor':'none'})
+'''
+
 plt.show()
 
