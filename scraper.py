462867ec0d6710848dffdb665b5da72b58f6444d
Ziemek <ziemowit.baczewski@gmail.com>
2024-01-29 17:12:58 +0100

    add more properties to scrape and change default properties to all

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ BASE_URL = "https://www.otodom.pl"
 LISTINGS_URL = BASE_URL + "/pl/wyniki/sprzedaz/mieszkanie/dolnoslaskie/wroclaw"
 
 # all available properties of properties
-PROPERTIES=['area', 'price', 'district', 'rent', 'market', 'garage', 'balcony']
+PROPERTIES=['area', 'price', 'rent', 'district', 'latitude', 'longitude', 'market', 'build_year', 'garage', 'lift', 'basement', 'balcony_garden_terrace']
 
 def retrieve_listing_data(url, properties):
     listing = requests.get(url)
@@ -26,7 +26,7 @@ def retrieve_listing_data(url, properties):
         return None
 
     try:
-        props_dict = dict()
+        props_dict = {p: None for p in properties}
 
         data = json.loads(json_data)
         props = data.get('props', {})
@@ -41,42 +41,71 @@ def retrieve_listing_data(url, properties):
         # extract 'target'; fields which interest us the most
         target = ad_data.get('target')
         
-        props_dict['area'] = float(target.get('Area', 0))
-        #props_dict['build_year'] = int(target.get('Build_year', 0)) 
+        props_dict['area'] = float(target.get('Area', None))
         #props_dict['floors_num'] = int(target.get('Building_floors_num', 0))
         #props_dict['floor_num'] = target.get('Floor_no') # keep in mind strange formating e.g. ["floor_5"]
-        props_dict['price'] = target.get('Price', 0)
-        props_dict['rent'] = target.get('Rent', 0)
+        props_dict['price'] = target.get('Price', None)
+        props_dict['rent'] = target.get('Rent', None)
         
         location = ad_data.get('location')
         coords = location.get('coordinates')
 
-        latitude = coords.get('latitude', 0.0)
-        longitude = coords.get('longitude', 0.0)
+        latitude = coords.get('latitude', None)
+        longitude = coords.get('longitude', None)
         props_dict['district'] = classify_coords(latitude, longitude)
+        props_dict['latitude'] = latitude
+        props_dict['longitude'] = longitude
         characteristics = ad_data.get('characteristics')
         market = None # secondary or primary -- wtórny lub pierwotny
         for c in characteristics:
             if c.get('key', None) == "market":
                 props_dict['market'] = c.get('value', None)
                 break
+        
+        # build year
+        build_year = target.get('Build_year', None)
+        props_dict['build_year'] = int(build_year) if build_year != None else None 
+
+        # constructions status: "ready_to_use", "to_completion", "to_renovation" or None
+        # stan wykończenia odpowiednio: "do zamieszkania", "do wykończenia", "do remontu"
+        construction_status = target.get('Construction_status', None)
+        if construction_status:
+            props_dict['construction_status'] = construction_status[0]
+
+        # informacje dodatkowe
+        extras = target.get('Extras_types', None)
+        if extras:
+            props_dict['garage']   = True if "garage" in extras else False
+            props_dict['lift']     = True if "lift" in extras else False
+            props_dict['basement'] = True if "basement" in extras else False
+            props_dict['balcony_garden_terrace'] = []
+            if "balcony" in extras:
+                props_dict['balcony_garden_terrace'].append("balcony")
+            if "garden" in extras:
+                props_dict['balcony_garden_terrace'].append("garden")
+            if "terrace" in extras:
+                props_dict['balcony_garden_terrace'].append("terrace")
+            if not props_dict['balcony_garden_terrace']:
+                props_dict['balcony_garden_terrace'] = None
+
 
         print(f"ID: {ad_id}")
         #print(f"Description: {ad_description}")
         print(f"Area: {props_dict['area']}")
         print(f"Price: {props_dict['price']}")
-        #print(f"Latitude: {latitude}")
-        #print(f"Longitude: {longitude}")
+        print(f"Latitude: {latitude}")
+        print(f"Longitude: {longitude}")
         print(f"District: {props_dict['district']}")
         print(f"Rent: {props_dict['rent']}")
         print(f"Market: {props_dict['market']}")
+        input()
         
         data = [props_dict[p] for p in properties]
 
-        return data if all(data) else None
+        #return data if all(data) else None
+        return data
     except AttributeError as e:
         print(f"error when scraping JSON {e}")
-        input()
         return None
 
     return None
@@ -94,7 +123,7 @@ def get_max_page(listings_url):
 def scrape_otodom(properties=None, listings_mx=None):
 
     if not properties:
-        properties=PROPERTIES[:2]
+        properties=PROPERTIES
     else:
         properties = sorted(properties, key=lambda x: PROPERTIES.index(x))
     if not listings_mx:
@@ -125,7 +154,6 @@ def scrape_otodom(properties=None, listings_mx=None):
                     print("Reached the given limit")
                     return None 
         print("Reached the end of the listings")
-            #input()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Scrapes otodom website for flat listings")
